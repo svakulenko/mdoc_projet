0b917f1c52750722743e869d5694764067a91502
Sergey Vakulenko <ppdlive@gmail.com>
2011-12-26 16:04:28 +0100

    hibernate spring integration2

---
+++ src/dbHandlers/DAOHibernateManager.java
@@ -0,0 +1,16 @@
+package dbHandlers;
+
+import org.hibernate.SessionFactory;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+
+public class DAOHibernateManager {
+	private HibernateTemplate hibernateTemplate;
+	
+	public HibernateTemplate getHibernateTemplate(){
+		return hibernateTemplate;
+	}
+	
+    public void setSessionFactory(SessionFactory sessionFactory) {   
+          this.hibernateTemplate = new HibernateTemplate(sessionFactory);   
+    } 
+}
