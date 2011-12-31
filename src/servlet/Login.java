4e982b182b1bb267d7ab4549bd181f3e509720ed
Nguyen Xuan Hoa <nxhoaf@gmail.com>
2011-12-31 17:05:41 +0100

    refactoring, changing variable name

--- src/servlet/Login.java
+++ src/servlet/Login.java
@@ -41,48 +41,41 @@ public class Login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Login::doPost");
 		@SuppressWarnings("unchecked")
-		Enumeration<String> prms = request.getParameterNames();
-		if (prms.hasMoreElements()) {
-			String prmName = (String) prms.nextElement();
-			System.out.println("enum par name = " + prmName ); //why only password?
-		}
+//		Enumeration<String> prms = request.getParameterNames();
+//		if (prms.hasMoreElements()) {
+//			String prmName = (String) prms.nextElement();
+//			System.out.println("enum par name = " + prmName ); //why only password?
+//		}
 		
-	    String lgn = request.getParameter("username");
-		String pwd = request.getParameter("password");
+	    String loginName = request.getParameter("username");
+		String password = request.getParameter("password");
 	
 
 			
 		
-		System.out.println("user=" + lgn + 
-				", pwd=" + pwd +
-				", methode =" +request.getMethod() +
-				", parametersNames=" );
-		
-		System.out.println("getServletContext().getContextPath()=" + getServletContext().getContextPath());
+//		System.out.println("user=" + loginName + 
+//				", pwd=" + password +
+//				", methode =" +request.getMethod() +
+//				", parametersNames=" );
+//		
+//		System.out.println("getServletContext().getContextPath()=" + getServletContext().getContextPath());
 		
 		
 		RequestDispatcher rd = null;
 		
 
 
-		
+		String path = "";
 		//jsp variant
 		//on ajoute le parameter supplementaire pour g�n�rer dynamiquement link dans error page
-		String[] ParError = {"indexPage", getServletContext().getContextPath() + "/index.html"};
-		String[] ParSuccess = {"acceuil", getServletContext().getContextPath() + "/accueil.jsp"};
-		if (lgn == null || lgn == "" 
-				|| pwd == ""|| pwd == null 
-				|| !lgn.equals(pwd)
-				)
-			rd = getServletContext().getRequestDispatcher("/loginError.jsp"
-				+ serverUtils.getNewParameter(ParError[0],ParError[1])
-				);
+		String[] errorPage = {"indexPage", getServletContext().getContextPath() + "/index.html"};
+		String[] successPage = {"acceuil", getServletContext().getContextPath() + "/accueil.jsp"};
+		
+		if (loginName.isEmpty()| !loginName.equals(password) )
+			path = "/loginError.jsp" + serverUtils.getNewParameter(errorPage[0],errorPage[1]);
 		else
-			rd = getServletContext().getRequestDispatcher("/loginSuccess.jsp"
-					+ serverUtils.getNewParameter(ParSuccess[0],ParSuccess[1])
-					);
-		
-		
+			path = "/loginSuccess.jsp" + serverUtils.getNewParameter(successPage[0],successPage[1]);
+		rd = getServletContext().getRequestDispatcher(path);
 		rd.forward(request, response);
 		
 		
