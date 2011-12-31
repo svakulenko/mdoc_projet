01124878c2def89e36112f2685bc9a7d067fff98
Nguyen Xuan Hoa <nxhoaf@gmail.com>
2011-12-31 22:26:37 +0100

    updating...

---
+++ src/daoImpl/DAOEntreprise.java
@@ -0,0 +1,52 @@
+package daoImpl;
+
+import java.util.Set;
+
+import mydomain.Entreprise;
+import daoInterface.IDAOEntreprise;
+
+public class DAOEntreprise implements IDAOEntreprise 
+{
+	private Entreprise entreprise;
+	@Override
+	public Entreprise getEntreprise() 
+	{
+		// TODO Auto-generated method stub
+		return this.entreprise;
+	}
+
+	@Override
+	public void setEntreprise(Entreprise entreprise) 
+	{
+		// TODO Auto-generated method stub
+		this.entreprise = entreprise;
+	}
+
+	@Override
+	public void addEntreprise(long id, String FirstName, String LastName,
+			String Email, int siretNumber) 
+	{
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	@Override
+	public Entreprise searchEntreprise(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entreprise deleteEntreprise(long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Entreprise> getAllEntreprise() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/daoInterface/IDAOEntreprise.java
@@ -0,0 +1,28 @@
+package daoInterface;
+
+import java.util.Set;
+
+import mydomain.Entreprise;
+
+public interface IDAOEntreprise 
+{
+	public Entreprise getEntreprise();
+	public void setEntreprise(Entreprise entreprise);
+	public void addEntreprise(long id, String FirstName, String LastName,String Email, int siretNumber);
+	public Entreprise searchEntreprise(long id);
+	public Entreprise deleteEntreprise(long id);
+	public Set<Entreprise> getAllEntreprise();
+}
+
+
+/*
+
+public void setContact(Contact contact);
+	public Contact getContact();
+	public String hClearTable();
+	public String hAddContact(long id, String FirstName, String LastName,String Email);
+	public String hSearchContact(long id);
+	public String deleteContact(long id);
+	public String getAllContacts();
+
+*/
