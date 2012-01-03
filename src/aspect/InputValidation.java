b56071ca3fe2459a0275c0e703dd785af1139903
Nguyen Xuan Hoa <nxhoaf@gmail.com>
2012-01-03 12:16:34 +0100

    add InputValidation aspect

---
+++ src/aspect/InputValidation.java
@@ -0,0 +1,13 @@
+package aspect;
+
+public class InputValidation 
+{
+	public void validateBefore()
+	{
+		System.out.println("Validate before");
+	}
+	public void validateAfter()
+	{
+		System.out.println("Validate after");
+	}
+}
