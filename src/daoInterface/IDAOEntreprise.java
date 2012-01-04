99cb11f385a610cf267b6db04785f1f5e1af9d73
Nguyen Xuan Hoa <nxhoaf@gmail.com>
2012-01-05 00:48:39 +0100

    refactoring

--- src/daoInterface/IDAOEntreprise.java
+++ src/daoInterface/IDAOEntreprise.java
@@ -54,16 +54,3 @@ public interface IDAOEntreprise
 			String numSiret
 		 );
 }
-
-
-/*
-
-public void setContact(Contact contact);
-	public Contact getContact();
-	public String hClearTable();
-	public String hAddContact(long id, String FirstName, String LastName,String Email);
-	public String hSearchContact(long id);
-	public String deleteContact(long id);
-	public String getAllContacts();
-
-*/
